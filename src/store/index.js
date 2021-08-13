3c8f26438b9d6c8c677ce8f3a6cec315da7a6170
zaqw60 <79165490+zaqw60@users.noreply.github.com>
2021-08-14 00:50:29 +0300

    Add files via upload
    lesson-5

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from "redux";
+import { profileReducer } from "./profile";
+
+export const store = createStore(
+  combineReducers({
+    profile: profileReducer,
+  })
+);
