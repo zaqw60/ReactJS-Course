40fefb0afa01ce6e422a9abf83deef0aad3674a6
zaqw60 <79165490+zaqw60@users.noreply.github.com>
2021-08-22 22:12:03 +0300

    Add files via upload

---
+++ src/components/header/menu/menu.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const menu = [
+  { to: "/", name: "Главная" },
+  { to: "/chat", name: "Чат" },
+]
+
+export function Menu() {
+  return (
+    <ul className="menu">
+      {menu.map((item) => (
+        <li key={item.name}>
+          <Link color="inherit" to={item.to}>
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
--- src/components/messageList/message/message.js
+++ src/components/messageList/message/message.js
@@ -3,7 +3,7 @@ export function Message({ author, message }) {
     <div className="messageItem">
       <h3>{message}</h3>
       <p>{author}</p>
-      <p>12.03</p>
+      <p>12.3</p>
     </div>
   )
 }
